a8737db0be25b9157ed5ae8ab047a97ac68f4c2c
Alec Meyer <11meyal@gmail.com>
2024-07-08 18:13:32 -0400

    Add new Basic Web API project with Lambda support
    This commit creates a new AWS Serverless ASP.NET Core Web API project. The Web API is hosted on AWS Lambda and is exposed through Amazon API Gateway. Additions include setup files, test files, gitignore, and important project files like Startup.cs and LambdaEntryPoint.cs. The Web API has a default ValuesController and also a startup test setup with a sample request.

---
+++ ExampleLambdaWebApi/src/ExampleLambdaWebApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace ExampleLambdaWebApi;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapGet("/", async context =>
+            {
+                var result = Results.Json(new
+                {
+                    Message = "Hello Robby!"
+                });
+
+                await result.ExecuteAsync(context);
+            });
+        });
+    }
+}
